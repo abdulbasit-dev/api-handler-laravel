38d70d67168845c93130d2d8ad9e5c49ea6f1df9
abdulbasit-web <abdullbasit99salah@gmail.com>
2021-01-25 17:51:20 +0300

    handle login with an error

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,42 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Psr7\Message;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+
+
+        //validate request
+        $validator = Validator::make($request->all(), [
+            "email" => "required|email",
+            "password" => "required|min:8",
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(["error" => true, "message" => $validator->errors()], 400);
+        }
+
+        $credentials = request(["email", "password"]);
+        $token = auth("api")->attempt($credentials);
+
+        if (!$token) {
+            return response()->json(["error" => true, "message" => "Unautherized"], 401);
+        }
+
+        return response()->json([
+            "access_token" => $token,
+            "expire_in" => auth("api")->factory()->getTTL() * 3600
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        return "it work";
+    }
 }
